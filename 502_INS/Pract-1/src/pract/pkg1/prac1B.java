e2e9b3dbcc20ce8bed01b57f5765f291cd4859d1
TYCSRoyalCollege <datyboyz2023@gmail.com>
2023-07-04 12:20:38 +0530

    code snippet of B

--- 502_INS/Pract-1/src/pract/pkg1/prac1B.java
+++ 502_INS/Pract-1/src/pract/pkg1/prac1B.java
@@ -1,5 +1,6 @@
 package pract.pkg1;
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class prac1B {
     public static char a[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
@@ -32,6 +33,7 @@ public class prac1B {
     }
     
   public static void main(String[] args) {
+      System.out.println("Enter The String :");
       Scanner sc = new Scanner(System.in);
   System.out.println("Enter The String :");
   String str = sc.nextLine();
