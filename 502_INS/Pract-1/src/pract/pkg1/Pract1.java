f198aa05fdda99cbe6ad1f242eec478e51e1ddb7
TYCSRoyalCollege <datyboyz2023@gmail.com>
2023-07-05 12:24:26 +0530

    stqa practical1

--- 502_INS/Pract-1/src/pract/pkg1/Pract1.java
+++ 502_INS/Pract-1/src/pract/pkg1/Pract1.java
@@ -1,25 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pract.pkg1;
 import java.util.Scanner;
-/**
- *
- * @author admin
- */
 public class Pract1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
         int key;
         char ch;
         String message, encryptedMessage="";
-        
-        // TODO code application logic here
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter Plain Text");
         message=sc.nextLine();
---
+++ 503_STQA/Practical1/web/newindex.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : newindex
+    Created on : Jul 5, 2023, 11:42:00 AM
+    Author     : Administrator
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+        <title> Page</title>
+    <body>
+        <h1>Test Case IDe</h1>
+        <a href="index.html">Operation</a>
+    </body>
+</html>
+`
