e2581411a4c1b1a6f3e517caceed0a3de14ae03c
TYCSRoyalCollege <datyboyz2023@gmail.com>
2023-07-10 11:18:15 +0530

    game programming pratical

--- 504_GP/Practical3/Practical3/Form1.cs
+++ 504_GP/Practical3/Practical3/Form1.cs
@@ -6,14 +6,54 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
 
 namespace Practical3
 {
     public partial class Form1 : Form
     {
+        Microsoft.DirectX.Direct3D.Device device;
+        Microsoft.DirectX.Direct3D.Texture texture;
+        Microsoft.DirectX.Direct3D.Font font;
         public Form1()
         {
             InitializeComponent();
+            InitDevice();
+            InitFont();
+            LoadTexture();
+        }
+
+        private void InitFont()
+        {
+            System.Drawing.Font f = new
+            System.Drawing.Font("Arial",16f,FontStyle.Regular);
+            font = new Microsoft.DirectX.Direct3D.Font(device, f);
+        }
+
+        private void LoadTexture()
+        {
+            texture = TextureLoader.FromFile(device, "D:\\TYCS_Faizan\\504_GP\\Practical3\\Practical3\\map.png", 400, 400, 1, 0, Format.A8B8G8R8, Pool.Managed, Filter.Point, Filter.Point, Color.Transparent.ToArgb());
+        }
+
+        private void InitDevice()
+        {
+            PresentParameters pp = new PresentParameters();
+            pp.Windowed = true;
+            pp.SwapEffect = SwapEffect.Discard;
+            device = new Device(0, DeviceType.Hardware, this, CreateFlags.SoftwareVertexProcessing, pp);
+        }
+        private void Render()
+        {
+            device.Clear(ClearFlags.Target, Color.Cyan, 0, 1);
+            device.BeginScene();
+            using (Sprite s = new Sprite(device))
+                s.Begin(SpriteFlags.AlphaBlend);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
