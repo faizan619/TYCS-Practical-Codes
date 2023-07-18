b6f79ba7d4acbb5c8a6064641d9f4b893f846ef4
TYCSRoyalCollege <datyboyz2023@gmail.com>
2023-07-18 11:19:52 +0530

    ins practical added

---
+++ 502_INS/Pract-1/src/pract/pkg1/Practical2.java
@@ -0,0 +1,14 @@
+package pract.pkg1;
+import java.util.Scanner;
+
+public class Practical2 {
+    public static void main(String[] args) {
+        int q,p;
+        String text;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a plain Text");
+        text = sc.nextLine();
+        System.out.println("Your text is :"+text);
+        
+    }
+}
